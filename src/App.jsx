1fcbac54dcd1ed9f39868b53d1baa993c4c85385
gudaghers <gudaghers@gmail.com>
2025-02-05 14:53:48 +0530

    resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Loading from './components/loading';
 import Navbar from './components/navbar';
 
-const Contacts = React.lazy(() => import('./pages/contacts'));
-const Education = React.lazy(() => import('./pages/education'));
-const Home = React.lazy(() => import('./pages/home/index'));
-const Portfolio = React.lazy(() => import('./pages/portfolio'));
-const Resume = React.lazy(() => import('./pages/resume'));
-const Skills = React.lazy(() => import('./pages/skills'));
+const Contacts = lazy(() => import('./pages/contacts'));
+const Education = lazy(() => import('./pages/education'));
+const Home = lazy(() => import('./pages/home/index'));
+const Portfolio = lazy(() => import('./pages/portfolio'));
+const Resume = lazy(() => import('./pages/resume'));
+const Skills = lazy(() => import('./pages/skills'));
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <React.Suspense fallback={<Loading />}>
+      <Suspense fallback={<Loading />}>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/MyResume" component={Home} />
           <Route path="/resume" component={Resume} />
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/skills" component={Skills} />
           <Route path="/education" component={Education} />
           <Route path="/contacts" component={Contacts} />
         </Switch>
-      </React.Suspense>
-    </>
+      </Suspense>
+    </Router>
   );
 };
 
